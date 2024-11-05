65815e4f8ec26a46496283e243772817f7cee494
Morten <mofb@itu.dk>
2024-11-05 21:01:01 +0100

    fixed proto

--- grpc/proto_grpc.pb.go
+++ grpc/proto_grpc.pb.go
@@ -18,7 +18,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type HomeworkFourServiceClient interface {
-	SendTokenToNextCLient(ctx context.Context, opts ...grpc.CallOption) (HomeworkFourService_SendTokenToNextCLientClient, error)
+	SendTokenToNextCLient(ctx context.Context, in *TokenSendRequest, opts ...grpc.CallOption) (*TokenSendResponse, error)
 }
 
 type homeworkFourServiceClient struct {
@@ -29,45 +29,20 @@ func NewHomeworkFourServiceClient(cc grpc.ClientConnInterface) HomeworkFourServi
 	return &homeworkFourServiceClient{cc}
 }
 
-func (c *homeworkFourServiceClient) SendTokenToNextCLient(ctx context.Context, opts ...grpc.CallOption) (HomeworkFourService_SendTokenToNextCLientClient, error) {
-	stream, err := c.cc.NewStream(ctx, &HomeworkFourService_ServiceDesc.Streams[0], "/HomeworkFourService/SendTokenToNextCLient", opts...)
+func (c *homeworkFourServiceClient) SendTokenToNextCLient(ctx context.Context, in *TokenSendRequest, opts ...grpc.CallOption) (*TokenSendResponse, error) {
+	out := new(TokenSendResponse)
+	err := c.cc.Invoke(ctx, "/HomeworkFourService/SendTokenToNextCLient", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &homeworkFourServiceSendTokenToNextCLientClient{stream}
-	return x, nil
-}
-
-type HomeworkFourService_SendTokenToNextCLientClient interface {
-	Send(*TokenSend) error
-	CloseAndRecv() (*Empty, error)
-	grpc.ClientStream
-}
-
-type homeworkFourServiceSendTokenToNextCLientClient struct {
-	grpc.ClientStream
-}
-
-func (x *homeworkFourServiceSendTokenToNextCLientClient) Send(m *TokenSend) error {
-	return x.ClientStream.SendMsg(m)
-}
-
-func (x *homeworkFourServiceSendTokenToNextCLientClient) CloseAndRecv() (*Empty, error) {
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	m := new(Empty)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
+	return out, nil
 }
 
 // HomeworkFourServiceServer is the server API for HomeworkFourService service.
 // All implementations must embed UnimplementedHomeworkFourServiceServer
 // for forward compatibility
 type HomeworkFourServiceServer interface {
-	SendTokenToNextCLient(HomeworkFourService_SendTokenToNextCLientServer) error
+	SendTokenToNextCLient(context.Context, *TokenSendRequest) (*TokenSendResponse, error)
 	mustEmbedUnimplementedHomeworkFourServiceServer()
 }
 
@@ -75,8 +50,8 @@ type HomeworkFourServiceServer interface {
 type UnimplementedHomeworkFourServiceServer struct {
 }
 
-func (UnimplementedHomeworkFourServiceServer) SendTokenToNextCLient(HomeworkFourService_SendTokenToNextCLientServer) error {
-	return status.Errorf(codes.Unimplemented, "method SendTokenToNextCLient not implemented")
+func (UnimplementedHomeworkFourServiceServer) SendTokenToNextCLient(context.Context, *TokenSendRequest) (*TokenSendResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SendTokenToNextCLient not implemented")
 }
 func (UnimplementedHomeworkFourServiceServer) mustEmbedUnimplementedHomeworkFourServiceServer() {}
 
@@ -91,30 +66,22 @@ func RegisterHomeworkFourServiceServer(s grpc.ServiceRegistrar, srv HomeworkFour
 	s.RegisterService(&HomeworkFourService_ServiceDesc, srv)
 }
 
-func _HomeworkFourService_SendTokenToNextCLient_Handler(srv interface{}, stream grpc.ServerStream) error {
-	return srv.(HomeworkFourServiceServer).SendTokenToNextCLient(&homeworkFourServiceSendTokenToNextCLientServer{stream})
-}
-
-type HomeworkFourService_SendTokenToNextCLientServer interface {
-	SendAndClose(*Empty) error
-	Recv() (*TokenSend, error)
-	grpc.ServerStream
-}
-
-type homeworkFourServiceSendTokenToNextCLientServer struct {
-	grpc.ServerStream
-}
-
-func (x *homeworkFourServiceSendTokenToNextCLientServer) SendAndClose(m *Empty) error {
-	return x.ServerStream.SendMsg(m)
-}
-
-func (x *homeworkFourServiceSendTokenToNextCLientServer) Recv() (*TokenSend, error) {
-	m := new(TokenSend)
-	if err := x.ServerStream.RecvMsg(m); err != nil {
+func _HomeworkFourService_SendTokenToNextCLient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TokenSendRequest)
+	if err := dec(in); err != nil {
 		return nil, err
 	}
-	return m, nil
+	if interceptor == nil {
+		return srv.(HomeworkFourServiceServer).SendTokenToNextCLient(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/HomeworkFourService/SendTokenToNextCLient",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(HomeworkFourServiceServer).SendTokenToNextCLient(ctx, req.(*TokenSendRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 // HomeworkFourService_ServiceDesc is the grpc.ServiceDesc for HomeworkFourService service.
@@ -123,13 +90,12 @@ func (x *homeworkFourServiceSendTokenToNextCLientServer) Recv() (*TokenSend, err
 var HomeworkFourService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "HomeworkFourService",
 	HandlerType: (*HomeworkFourServiceServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams: []grpc.StreamDesc{
+	Methods: []grpc.MethodDesc{
 		{
-			StreamName:    "SendTokenToNextCLient",
-			Handler:       _HomeworkFourService_SendTokenToNextCLient_Handler,
-			ClientStreams: true,
+			MethodName: "SendTokenToNextCLient",
+			Handler:    _HomeworkFourService_SendTokenToNextCLient_Handler,
 		},
 	},
+	Streams:  []grpc.StreamDesc{},
 	Metadata: "grpc/proto.proto",
 }
