60076e884a0f4336d1d7a49fef57aa983890a0ab
Morten <mofb@itu.dk>
2024-11-07 11:54:40 +0100

    Switched from to go-files to one. Now its possible to have three nodes in three different terminals
    Co-authored-by: nalm2812 <nalm2812@users.noreply.github.com>
    Co-authored-by: kilzor33 <kilzor33@users.noreply.github.com>

--- nodeTwo/node.go
+++ nodeTwo/node.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	proto "HW4/grpc"
+	"context"
 	"fmt"
+	"log"
+	"math/rand/v2"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -16,31 +20,30 @@ type Node struct {
 	NextNodeID int32
 	ownPort    string
 	nextPort   string
+	hasToken   int
 }
 
-var nodes = []*Node{}
-var hasToken bool
+var hasToken int = 0
+var requestToken bool = false
 
-func (node *Node) ConnectToNextNode() error {
-	conn, err := grpc.Dial(node.nextPort, grpc.WithInsecure())
-	if err != nil {
-		return err
+//var nodes = []*Node{}
+
+func (node *Node) SendTokenToNextCLient(ctx context.Context, req *proto.TokenSendRequest) (*proto.TokenSendResponse, error) {
+	// Implement the method logic here
+
+	if req.Token == 1 {
+		hasToken = int(req.Token)
 	}
 
-	nextNode = proto.NewHomeworkFourServiceClient(conn)
-	fmt.Println(string(node.NodeID) + " Connected to next node " + string(node.NextNodeID))
-
-	return nil
-}
-
-func (node *Node) SendTokenToNextCLient(stream proto.HomeworkFourService_SendTokenToNextCLientServer) error {
-
-	// Implement the method logic here
-	return nil
+	return &proto.TokenSendResponse{
+		Success: true,
+	}, nil
 }
 
 func (node *Node) startNode() error { //maybe rename to StartServer for more clarity
 	listener, err := net.Listen("tcp", node.ownPort)
+
+	log.Printf("Node %d started", node.NodeID)
 
 	if err != nil {
 		return err
@@ -57,66 +60,94 @@ func (node *Node) startNode() error { //maybe rename to StartServer for more cla
 	return nil
 }
 
-func main() {
-	// Create the nodes
-	if len(nodes) == 0 {
-		nodeA := &Node{
-			NodeID:     0,
-			NextNodeID: 1,
-			ownPort:    ":5050",
-			nextPort:   ":5051",
-		}
+func (node *Node) ConnectToNextNode() {
+	log.Printf("Node %d is connecting to the next node", node.NodeID)
 
-		nodes = append(nodes, nodeA)
-		nodeA.startNode()
+	conn, err := grpc.Dial(node.nextPort, grpc.WithInsecure(), grpc.WithBlock())
 
-	} else if len(nodes) == 1 {
-		nodeB := &Node{
-			NodeID:     1,
-			NextNodeID: 2,
-			ownPort:    ":5051",
-			nextPort:   ":5052",
-		}
-
-		nodes = append(nodes, nodeB)
-		nodeB.startNode()
-
-	} else if len(nodes) == 2 {
-		nodeC := &Node{
-			NodeID:     2,
-			NextNodeID: 0,
-			ownPort:    ":5052",
-			nextPort:   ":5050",
-		}
-		nodes = append(nodes, nodeC)
-		nodeC.startNode()
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
 	}
+	nextNode = proto.NewHomeworkFourServiceClient(conn)
 }
 
-/*
-	// Create the nodes "automatically"
-	for i := 0; i < numberNodes; i++ {
-		if i == numberNodes-1 {
-			node := &Node{
-				NodeID:      int32(i),
-				NextNodeID:  0,
-				ownPort:     ":505" + string(i),
-				nextPort:    ":5050",
-			}
-			nodes = append(nodes, node)
-		} else {
-			node := &Node{
-				NodeID:      int32(i),
-				NextNodeID:  int32(i + 1),
-				ownPort:     ":505" + string(i),
-				nextPort:    ":505" + string(i+1),
-			}
-			nodes = append(nodes, node)
-		}
+func main() {
+	// Create the nodes
+
+	//maybe scan for ports, then make the nodes
+	var thisPort string
+	fmt.Scanln(&thisPort)
+
+	var thisNode *Node
+
+	if thisPort == "5050" {
+		thisNode = &Node{NodeID: 0, NextNodeID: 1, ownPort: ":5050", nextPort: ":5051", hasToken: 1}
+
+		hasToken = 1
+
+		log.Println("Node A created")
+		go thisNode.startNode()
+		thisNode.ConnectToNextNode()
+
 	}
 
-	// Run the nodes
-	for _, node := range nodes {
-		go node.startNode()
+	if thisPort == "5051" {
+		thisNode = &Node{NodeID: 1, NextNodeID: 2, ownPort: ":5051", nextPort: ":5052", hasToken: 0}
+		log.Println("Node B created")
+		go thisNode.startNode()
+		thisNode.ConnectToNextNode()
+
 	}
-*/
+
+	if thisPort == "5052" {
+		thisNode = &Node{NodeID: 2, NextNodeID: 0, ownPort: ":5052", nextPort: ":5050", hasToken: 0}
+
+		log.Println("Node C created")
+		go thisNode.startNode()
+		thisNode.ConnectToNextNode()
+
+	}
+
+	for {
+		if !requestToken {
+			var random = rand.IntN(4)
+			if random == 1 {
+				requestToken = true
+				log.Printf("Node %d is requesting the token", thisNode.NodeID)
+			}
+		}
+		if nextNode != nil {
+			if hasToken == 1 {
+				log.Printf("Node %d has the token", thisNode.NodeID)
+				if requestToken {
+					log.Printf("Node %d is using the toke to access the critical section", thisNode.NodeID)
+					time.Sleep(2 * time.Second)
+					requestToken = false
+				}
+				_, err := nextNode.SendTokenToNextCLient(context.Background(), &proto.TokenSendRequest{
+					Message:  "now you have the token",
+					Token:    1,
+					SenderID: thisNode.NodeID,
+				})
+				hasToken = 0
+				if err != nil {
+					log.Fatalf("Failed to send token to next node: %v", err)
+				}
+				log.Printf("Node %d sends token to %d", thisNode.NodeID, thisNode.NextNodeID)
+
+			} else {
+				_, err := nextNode.SendTokenToNextCLient(context.Background(), &proto.TokenSendRequest{
+					Message:  "you dont have the token",
+					Token:    0,
+					SenderID: thisNode.NodeID,
+				})
+				if err != nil {
+					log.Fatalf("Failed to send token to next node: %v", err)
+				}
+
+				//log.Printf("Node %d sends message to %d", thisNode.NodeID, thisNode.NextNodeID)
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}
+}
